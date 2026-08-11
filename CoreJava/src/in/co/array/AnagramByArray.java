966aebce8b5552be8ef5b6b08fe75c37132c6ec2
Karunachouhan <karunachouhan0001@gmail.com>
2026-08-11 11:28:27 +0530

    Anagram done

---
+++ CoreJava/src/in/co/array/AnagramByArray.java
@@ -0,0 +1,23 @@
+package in.co.array;
+
+import java.util.Arrays;
+
+public class AnagramByArray {
+	public static void main(String[] args) {
+		String str = "listen";
+		String str1 = "silent";
+
+		char[] a = str.toCharArray(); // l,i,s,t,e,n
+		char[] b = str1.toCharArray();
+
+		Arrays.sort(a);
+		Arrays.sort(b);
+		
+		if(Arrays.equals(a,b)) {
+				System.out.println("It is anagram");
+		}else {
+			System.out.println("Not anagram");
+		}
+
+	}
+}
